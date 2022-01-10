c6cfbe6090b17ced195b910c7ed5d2a9df9e18dc
timokluser-dev <timo@timokluser.ch>
2022-01-10 09:53:42 +0100

    feat: add google font & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Roboto', 'sans-serif'],
+        'mono': ['"Roboto Mono"', 'monospace'],
+        'serif': ['"Roboto Slab"', 'serif']
+      },
+    },
   },
   variants: {
     extend: {},
