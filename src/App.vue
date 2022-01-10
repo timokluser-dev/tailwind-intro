10452900346d2a652e6fd762f0ab6898d3f4724a
timokluser-dev <timo@timokluser.ch>
2022-01-10 11:08:52 +0100

    feat: add app bg image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
-  <div id="app" class="bg-gray-50 w-screen h-screen">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="bg-gray-50">
+    <div class="app__nav fixed w-full bg-gray-50">
+      <div id="nav" class="nav">
+        <router-link to="/">Home</router-link>
+        <span class="nav__separator">|</span>
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
-    <router-view/>
+    <div class="app__background w-full h-full min-h-screen"></div>
+    <div class="app__content h-4/5 py-10">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+.app {
+  &__background {
+    background-image: url("assets/app-bg.jpg");
+    @apply bg-no-repeat bg-cover bg-bottom bg-fixed;
+  }
+}
+
+.nav {
+  @apply align-middle;
+
+  &__separator {
+    @apply px-1.5;
+  }
+}
+
 #app {
   @apply text-center;
   @apply text-vue-blue;
