f353a7263f1768094202eb70f29ef7f771b6ca37
timokluser-dev <timo@timokluser.ch>
2022-01-10 10:43:45 +0100

    feat: add height, width & app background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-gray-50 w-screen h-screen">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,10 +9,6 @@
 </template>
 
 <style lang="scss">
-body {
-  @apply bg-gray-50;
-}
-
 #app {
   @apply text-center;
   @apply text-vue-blue;
