202859e620d15a32eb129a376c785ff57db0cb8b
timokluser-dev <timo@timokluser.ch>
2022-01-10 15:06:39 +0100

    feat: add animations, transformations & transition on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button class="about__link" @click.prevent="onClick($event)">Click me</button>
   </div>
 </template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  // eslint-disable-next-line
+  onClick($event: any): void {
+    console.debug('button clicked', $event);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.about {
+  &__link {
+    @apply text-white bg-vue-blue rounded p-4 px-12;
+    @apply transform-gpu;
+    @apply transition-all duration-150 delay-75;
+
+    // same as `@apply hover:bg-vue-green`
+    &:hover {
+      @apply bg-vue-green;
+      @apply scale-110;
+    }
+  }
+}
+</style>
